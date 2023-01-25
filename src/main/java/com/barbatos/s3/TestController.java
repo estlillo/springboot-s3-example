c76239da323ecd7b8bc9281a17a15f21cfeff6d7
Barbatos <estebanlilloc@gmail.com>
2023-01-25 12:30:48 -0300

    Test s3 inicial

---
+++ src/main/java/com/barbatos/s3/TestController.java
@@ -0,0 +1,27 @@
+package com.barbatos.s3;
+
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    
+    @GetMapping("/test")
+    public String test(){
+        return "test";
+    }
+
+    @GetMapping("/uuid")
+    public String UUIDGenerator(){
+        
+        UUID uuid = UUID.randomUUID();
+        String uuidAsString = uuid.toString();
+
+        System.out.println("Your UUID is: " + uuidAsString);
+
+        return uuidAsString;
+    }
+
+}
